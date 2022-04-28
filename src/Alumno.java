32b1e8a37db07a246bcbc702c61ad11630539991
isma136 <sanz.ismael.2002@gmail.com>
2022-04-28 18:35:09 +0200

    Creamos constructor con parametros desde Ismael

--- src/Alumno.java
+++ src/Alumno.java
@@ -3,4 +3,13 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	public Alumno(String nombre, String apellido, String dni) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	
+	
 }
