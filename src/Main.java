5db9d137373dc070b7778d6744e454d687fe7a22
isma136 <sanz.ismael.2002@gmail.com>
2022-04-28 18:37:36 +0200

    Instanciamos el objeto desde master

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos DAW");
 		System.out.println("Alumno 1 :");
+		
+		Alumno alumno1 = new Alumno("Ismael","Sanz","12345678U");
+		System.out.println(alumno1);
 	}
 
 }
